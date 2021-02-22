e14df41bcb87dc9320b63333e4015bc9a398f12d
Matteo Borghini <matteo.borghini.web@gmail.com>
2021-02-22 17:07:44 +0100

    Aggiungo anche un JSON e un TS DA AGGIORNARE

---
+++ icons.ts
@@ -0,0 +1,154 @@
+export let icons = [
+    {
+        value: '&#xe908;',
+        title: 'Predefinita'
+    },
+    {
+        value: '&#xe900;',
+        title: 'Aereo'
+    },
+    {
+        value: '&#xe901;',
+        title: 'Ambulanza'
+    },
+    {
+        value: '&#xe902;',
+        title: 'Arbre Magique'
+    },
+    {
+        value: '&#xe903;',
+        title: 'Auto'
+    },
+    {
+        value: '&#xe904;',
+        title: 'Batteria'
+    },
+    {
+        value: '&#xe905;',
+        title: 'Bicicletta'
+    },
+    {
+        value: '&#xe906;',
+        title: 'Bus'
+    },
+    // {
+    //     value: '&#xe907;',
+    //     title: 'Cacca'
+    // },
+    {
+        value: '&#xe909;',
+        title: 'Camion 2'
+    },
+    {
+        value: '&#xe90a;',
+        title: 'Caravan'
+    },
+    {
+        value: '&#xe90b;',
+        title: 'Carrello Montacarichi'
+    },
+    {
+        value: '&#xe90c;',
+        title: 'Elicottero'
+    },
+    {
+        value: '&#xe90d;',
+        title: 'Jet'
+    },
+    {
+        value: '&#xe90e;',
+        title: 'LocationMappa'
+    },
+    {
+        value: '&#xe90f;',
+        title: 'MarkerMappa'
+    },
+    {
+        value: '&#xe910;',
+        title: 'MarkerMappa 2'
+    },
+    {
+        value: '&#xe911;',
+        title: 'Monster Truck'
+    },
+    {
+        value: '&#xe912;',
+        title: 'Motocicletta'
+    },
+    {
+        value: '&#xe913;',
+        title: 'Nave'
+    },
+    // {
+    //     value: '&#xe914;',
+    //     title: 'Persona Ceca'
+    // },
+    {
+        value: '&#xe915;',
+        title: 'Pickup'
+    },
+    {
+        value: '&#xe916;',
+        title: 'Pin Mappa'
+    },
+    {
+        value: '&#xe917;',
+        title: 'Pin Mappa 2'
+    },
+    {
+        value: '&#xe918;',
+        title: 'Razzo'
+    },
+    {
+        value: '&#xe919;',
+        title: 'Rimorchio'
+    },
+    {
+        value: '&#xe91a;',
+        title: 'Spalaneve'
+    },
+    {
+        value: '&#xe91b;',
+        title: 'Trattore'
+    },
+    {
+        value: '&#xe91c;',
+        title: 'Van'
+    },
+    {
+        value: '&#xe91d',
+        title: 'Piattaforma'
+    },
+    {
+        value: '&#xe91e',
+        title: 'Escavatore'
+    },
+    {
+        value: '&#xe91f',
+        title: 'MiniEscavatore'
+    },
+    {
+        value: '&#xe920',
+        title: 'MiniPala'
+    },
+    {
+        value: '&#xe921',
+        title: 'Muletto'
+    },
+    {
+        value: '&#xe922',
+        title: 'PalaGommata'
+    },
+    {
+        value: '&#xe923',
+        title: 'RulloCompattatore'
+    },
+    {
+        value: '&#xe924',
+        title: 'SollevatoreTelescopico'
+    },
+    {
+        value: '&#xe925',
+        title: 'Uomo'
+    }
+]
